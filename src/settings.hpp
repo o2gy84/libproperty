cb19038f75f8fb5091fbb2038d1b1a1e3ea09603
Victor Mogilin <o2gy84@gmail.com>
2017-01-10 05:31:31 -0500

    libproperty: transfer extended properties classes into separated file

--- src/settings.hpp
+++ src/settings.hpp
@@ -1,43 +1,10 @@
 #pragma once
 
-#include <string>
-#include <vector>
 #include <map>
 #include <memory>
 #include <iostream>
 
-
-namespace settings
-{
-    struct address_t
-    {
-        uint16_t port;
-        std::string host;
-        bool operator<(const address_t &rhs) const
-        {
-            if (host == rhs.host) return port < rhs.port;
-            return host < rhs.host;
-        }
-    };
-
-    struct file_t
-    {
-        std::string name;
-        std::string content;
-        bool operator<(const file_t &rhs) const
-        {
-            if (name == rhs.name) return content < rhs.content;
-            return name < rhs.name;
-        }
-    };
-
-    struct shard_t
-    {
-        std::vector<int> shards;
-        bool operator<(const shard_t &rhs) const { return shards < rhs.shards; }
-    };
-
-}
+#include "ext_properties.hpp"
 
 
 class AnyItem
